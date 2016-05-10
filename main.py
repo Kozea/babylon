ca8a8f8ab1a62a94a3893362362c7d5dac401091
Nicolas Bonfante <nicolas.bonfante@kozea.fr>
2016-05-10 17:22:27 +0200

    correction of some sql queries

--- main.py
+++ main.py
@@ -51,11 +51,10 @@ def new_match():
     request_team1 = g.db.execute('select * from teams where((id_player1=? and id_player2=?) or (id_player1=? and id_player2=?))',
                                         (request.form['id_player11'], request.form['id_player12'], request.form['id_player12'], request.form['id_player11']))                       
     # Creating team if doesn't exist
-    if (request_team1.fetchall()):
-        raise Exception ('SBRAAAAAH')
+    if (not request_team1.fetchall()):
         temp = g.db.execute('select max(id_team) from teams')
-        index = temp.fetchone()+1
-        g.db.execute('insert into teams (?,?,?,null)', (index, request.form['id_player11'], request.form['id_player12']))
+        index = temp.fetchone()[0]+1
+        g.db.execute('insert into teams values (?,?,?,null)', (index, request.form['id_player11'], request.form['id_player12']))
     
     #g.db.execute('insert into matchs (id_team1, id_team2, score_e1, score_e2) values (?, ?, ?, ?)',
      #            [request.form['id_team1'], request.form['id_team2'], request.form['score_e1'], request.form['score_e2']])
