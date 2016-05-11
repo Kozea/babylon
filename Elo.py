642f24c66b43892ce7913c22fee420140c7a7ec5
Nicolas Bonfante <nicolas.bonfante@kozea.fr>
2016-05-11 17:24:59 +0200

    merge. Deleting team table

--- Elo.py
+++ Elo.py
@@ -11,37 +11,37 @@ def new_score(old_elo_player1, old_elo_opponent1, old_elo_player2,
         # @score_player: the score of team1
         # @score_opponent: the score of team2
     """
-    if(number_of_matches < 30):
-        K = 40
-    elif(old_elo_player < 2400):
-        K = 20
-    else:
-        K = 10
-    
-    if(score_player > score_opponent):
-        W = 1
-    else:
-        W = 0
-        
-    D = 0
-    if(old_elo_opponent2 is None):
-        D -= old_elo_opponent1
-    else:
-        D -= (old_elo_opponent1 + old_elo_opponent2) / 2
-    
-    if(old_elo_player2 is None):
-        D += old_elo_player1
-    else:
-        D += (old_elo_player1 + old_elo_player2) / 2
-        
-    pD = p(D)
-    
-    new_score = old_elo_player1 + K * (W - pD)
-    return int(round(new_score,0))
-    #~ if(score_player > score_opponent):
-        #~ return old_elo_player1+10
+    #~ if(number_of_matches < 30):
+        #~ K = 40
+    #~ elif(old_elo_player < 2400):
+        #~ K = 20
     #~ else:
-        #~ return old_elo_player1-10
+        #~ K = 10
+    
+    #~ if(score_player > score_opponent):
+        #~ W = 1
+    #~ else:
+        #~ W = 0
+        
+    #~ D = 0
+    #~ if(old_elo_opponent2 is None):
+        #~ D -= old_elo_opponent1
+    #~ else:
+        #~ D -= (old_elo_opponent1 + old_elo_opponent2) / 2
+    
+    #~ if(old_elo_player2 is None):
+        #~ D += old_elo_player1
+    #~ else:
+        #~ D += (old_elo_player1 + old_elo_player2) / 2
+        
+    #~ pD = p(D)
+    
+    #~ new_score = old_elo_player1 + K * (W - pD)
+    #~ return int(round(new_score,0))
+    if(score_player > score_opponent):
+        return old_elo_player1+10
+    else:
+        return old_elo_player1-10
         
 def p(i):
     return 1/(1+10**(-i/400))
