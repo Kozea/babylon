9795b99e99b9e43e9f1bb604ffefdaa14ac47117
Nicolas Bonfante <nicolas.bonfante@kozea.fr>
2016-05-12 17:04:48 +0200

    flake8 database.py

--- database.py
+++ database.py
@@ -1,4 +1,9 @@
-from flask import *
+"""
+    This module contains all necessary config variable and common
+    useful variable. app for the flask instance and db for database
+    connection.
+"""
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 # configuration
