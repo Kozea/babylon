f7230e3c32106a7de6e62bc5def39b635018ccb2
Nicolas Bonfante <nicolas.bonfante@kozea.fr>
2016-05-12 17:22:10 +0200

    flake8 main.py

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 # all the imports
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash, send_from_directory
-from werkzeug import secure_filename
-from flask_sqlalchemy import SQLAlchemy
-import os
-import time
+from flask import request, render_template
 from PIL import Image
 from resizeimage import resizeimage
 from datetime import datetime
-
-# DO NOT DELETE OR MOVE THIS LINE
 from Model import User, Match
-from database import *
-   
+from database import db, app, ALLOWED_EXTENSIONS
+
+
 @app.route('/')
 def matchs():
     """Querying for all matchs in the database"""
     matchs = Match.query.all()
-    return render_template('match.html', matchs = matchs)
+    return render_template('match.html', matchs=matchs)
 
 
 @app.route('/ranking')
 def ranking():
     """Querying for the ranking"""
     unordered_ranking = compute_ranking()
-    ordered_ranking = sorted(unordered_ranking, key = lambda user: -user.ranking)
+    ordered_ranking = sorted(unordered_ranking,
+                             key = lambda user: -user.ranking)
     return render_template('ranking.html', users = ordered_ranking)
 
 
@@ -34,18 +28,18 @@ def add_match():
     """Allows user to access the match adding form."""
 
     users = User.query.all()
-      
+
     if(len(users) == 0):
         success = "There are no players yet !"
-        return render_template('add_match.html',success = success, user = True)
+        return render_template('add_match.html', success = success, user = True)
 
-    return render_template('add_match.html', users = users)   
+    return render_template('add_match.html', users = users)
 
 
 @app.route('/new_match', methods=['POST'])
 def new_match():
     """Creates a new match using values given to the add_match form"""
-        
+
     users = User.query.all()
 
     error = None
@@ -58,7 +52,7 @@ def new_match():
         error = 'Add a player 1 to team 2'
 
     # Check if some users appear twice
-    elif ((request.form['id_player11'] == request.form['id_player12']) or 
+    elif ((request.form['id_player11'] == request.form['id_player12']) or
           (request.form['id_player11'] == request.form['id_player21']) or
           (request.form['id_player11'] == request.form['id_player22']) or
           (request.form['id_player12'] == request.form['id_player21']) or
@@ -81,12 +75,12 @@ def new_match():
 
         score_e1 = request.form['score_e1']
         score_e2 = request.form['score_e2']
-                    
+
         player11 = User.query.filter_by(id_user=id_player11).first()
         player12 = User.query.filter_by(id_user=id_player12).first()
         player21 = User.query.filter_by(id_user=id_player21).first()
         player22 = User.query.filter_by(id_user=id_player22).first()
-                     
+
         match = Match(datetime.now(), score_e1, score_e2,
                       player11, player12, player21, player22)
 
@@ -110,7 +104,7 @@ def add_player():
         name = request.form['name']
         nickname = request.form['nickname']
 
-        filename =""
+        filename = ""
         # Get user photo and work on it
         photo = request.files['photo']
         if photo and allowed_file(photo.filename):
@@ -131,13 +125,8 @@ def add_player():
             if(cur):
                 error = "This nickname is already used !"
             else:
-              
                 # Add the new user to the database
-                
-                new_user = User(surname, name, nickname,filename)
-                #~ g.db.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?)",
-                             #~ (index, surname, name, nickname,
-                              #~ filename,))
+                new_user = User(surname, name, nickname, filename)
 
                 db.session.add(new_user)
                 db.session.commit()
@@ -165,11 +154,12 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+
 @app.route('/coucou')
 def coucou():
     toRet = ""
     matchs = Match.query.all()
-    
+
     for match in matchs:
         match.player11.set_ranking(1000)
         match.player12.set_ranking(1000)
@@ -179,17 +169,18 @@ def coucou():
         match.player12.set_number_of_matchs()
         match.player21.set_number_of_matchs()
         match.player22.set_number_of_matchs()
-        
+
     for match in matchs:
-        elo(match.player11, match.player12,match.player21,match.player22,
+        elo(match.player11, match.player12, match.player21, match.player22,
             match.score_e1, match.score_e2)
-            
+
     users = User.query.all()
     for user in users:
         toRet += str(user.ranking)
-        toRet+='<br>'
+        toRet += '<br>'
     return toRet
 # HELPER #
+
 
 def compute_ranking():
     users = User.query.all()
@@ -198,76 +189,76 @@ def compute_ranking():
         user.set_number_of_matchs()
 
     matchs = Match.query.all()
-    
+
     for match in matchs:
-        elo(match.player11, match.player12,match.player21,match.player22,
+        elo(match.player11, match.player12, match.player21, match.player22,
             match.score_e1, match.score_e2)
         print(match.score_e1, match.score_e2)
         print(match.player11.nickname, match.player21.nickname)
         print(match.player11.ranking, match.player21.ranking)
-        
-            
+
     users = User.query.all()
 
     return users
 
+
 def elo(me, my_friend, my_ennemy1, my_ennemy2, my_score, opponent_score):
     # Create fictive player1
     if(my_friend is None):
-        elo1 =  me.ranking
+        elo1 = me.ranking
         number_match1 = me.number_of_match
     else:
         elo1 = (me.ranking+my_friend.ranking)/2
         number_match1 = (me.number_of_match+my_friend.number_of_match)/2
-    
+
     # Create fictive player2
     if(my_ennemy2 is None):
-        elo2 =  my_ennemy1.ranking
+        elo2 = my_ennemy1.ranking
         number_match2 = my_ennemy1.number_of_match
     else:
         elo2 = (my_ennemy1.ranking+my_ennemy2.ranking)/2
         number_match2 = (my_ennemy1.number_of_match+my_ennemy2.number_of_match)/2
-    
-    
+
     # Score for player1
     We1 = p(my_score-opponent_score)
     K = chooseK(number_match1, elo1)
     G = chooseG(my_score, opponent_score)
     W = chooseW(my_score, opponent_score)
     score_p1 = K*G*(W-We1)
-    
+
     # Score for player2
     We2 = 1 - We1
     K = chooseK(number_match2, elo2)
     G = chooseG(opponent_score, my_score)
     W = chooseW(opponent_score, my_score)
     score_p2 = K*G*(W-We2)
-    
+
     # Get points to real users
     if(my_friend is None):
-        me.ranking += int(round(score_p1,0))
+        me.ranking += int(round(score_p1, 0))
         me.number_of_match += 1
     else:
         sum_ranking = me.ranking+my_friend.ranking
         # update my score
-        me.ranking += int(round((me.ranking/sum_ranking)*score_p1,0))
-        me.number_of_match += 1  
+        me.ranking += int(round((me.ranking/sum_ranking)*score_p1, 0))
+        me.number_of_match += 1
         # update my friend score
-        my_friend.ranking += int(round((my_friend.ranking/sum_ranking)*score_p1,0))
-        my_friend.number_of_match += 1  
-        
+        my_friend.ranking += int(round((my_friend.ranking/sum_ranking)*score_p1, 0))
+        my_friend.number_of_match += 1
+
     if(my_ennemy2 is None):
-        my_ennemy1.ranking += int(round((score_p2),0))
+        my_ennemy1.ranking += int(round((score_p2), 0))
         my_ennemy1.number_of_match += 1
     else:
         sum_ranking = my_ennemy1.ranking+my_ennemy2.ranking
         # update my score
-        my_ennemy1.ranking += int(round((my_ennemy1.ranking/sum_ranking)*score_p2,0))
-        my_ennemy1.number_of_match += 1  
+        my_ennemy1.ranking += int(round((my_ennemy1.ranking/sum_ranking)*score_p2, 0))
+        my_ennemy1.number_of_match += 1
         # update my friend score
-        my_ennemy2.ranking += int(round((my_ennemy2.ranking/sum_ranking)*score_p2,0))
-        my_ennemy2.number_of_match += 1  
-    
+        my_ennemy2.ranking += int(round((my_ennemy2.ranking/sum_ranking)*score_p2, 0))
+        my_ennemy2.number_of_match += 1
+
+
 def chooseK(number_of_match, elo):
     if(number_of_match < 40):
         return 40
@@ -275,7 +266,8 @@ def chooseK(number_of_match, elo):
         return 20
     else:
         return 10
-        
+
+
 def chooseG(score_e1, score_e2):
     diff = abs(score_e1 - score_e2)
     if(diff <2):
@@ -287,15 +279,18 @@ def chooseG(score_e1, score_e2):
     else:
         G = 1+3/4+(diff-3)/8
     return G
-    
+
+
 def chooseW(score_e1, score_e2):
     return 1 if score_e1 > score_e2 else 0
-    
+
+
 def p(i):
     return 1/(1+10**(-i/400))
-    
+
+
 def init_db():
     db.create_all()
-    
+
 if __name__ == '__main__':
     app.run()
