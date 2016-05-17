cbcbed097e19542a0b0b8cf0d6e26eae1b7e518d
Nicolas Bonfante <nicolas.bonfante@kozea.fr>
2016-05-17 12:04:41 +0200

    modifying add.sql

---
+++ add.sql
@@ -0,0 +1,9 @@
+insert into user values(0,"bonfante","nicolas","michel","photo1");
+insert into user values(1,"boscher","cedric","cedic","photo2");
+
+insert into match values(0,"2016-05-17 09:12:13.434915", 12,1,0,null,1,null);
+insert into match values(1,"2016-06-17 09:12:13.434915", 12,1,0,null,1,null);
+insert into match values(2,"2016-06-17 09:13:13.434915", 12,1,0,null,1,null);
+insert into match values(3,"2016-07-17 09:12:13.434915", 12,1,0,null,1,null);
+insert into match values(4,"2016-07-17 09:13:13.434915", 12,1,0,null,1,null);
+insert into match values(5,"2016-07-17 09:14:13.434915", 12,1,0,null,1,null);
