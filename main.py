99442cea533fbb2ddbffa377501172825cc8e5be
Nicolas Bonfante <nicolas.bonfante@kozea.fr>
2016-05-17 11:54:15 +0200

    flake 8 for main

--- main.py
+++ main.py
@@ -5,20 +5,15 @@ from resizeimage import resizeimage
 from datetime import datetime
 from Model import User, Match
 from database import db, app, ALLOWED_EXTENSIONS
-
-from sqlalchemy import func, or_
 import pygal
-from pygal.style import DarkSolarizedStyle
-
 import math
-from sqlalchemy.sql.expression import func
 from itertools import groupby
-from operator import itemgetter
 from copy import deepcopy
 from collections import OrderedDict
 
 
 player_tournament = 2
+
 
 @app.route('/')
 def matchs():
@@ -31,37 +26,37 @@ def matchs():
 def ranking():
     """Querying for the ranking"""
     unordered_ranking = compute_ranking()
-    
-    for user in unordered_ranking :
-        
+
+    for user in unordered_ranking:
         victories_as_team1 = db.session.query(Match.id_match).filter((Match.player11 == user) | (Match.player12 == user)).filter(Match.score_e1 > Match.score_e2).count()
         victories_as_team2 = db.session.query(Match.id_match).filter((Match.player21 == user) | (Match.player22 == user)).filter(Match.score_e1 < Match.score_e2).count()
 
         user.nb_victories = victories_as_team1 + victories_as_team2
         user.nb_defeats = user.number_of_match - user.nb_victories
-        
-        if user.number_of_match !=0 :
-        
-            gauge = pygal.SolidGauge(inner_radius=0.70, show_legend = False)
+
+        if user.number_of_match != 0:
+            gauge = pygal.SolidGauge(inner_radius=0.70, show_legend=False)
             gauge.value_formatter = lambda x: '{:.10g}%'.format(x)
             gauge.add('Ratio', [{'value': (user.nb_victories/user.number_of_match)*100, 'max_value': 100}])
-        
             user.ratio_gauge = gauge
-                                                      
-    
+
     ordered_ranking = sorted(unordered_ranking,
-                             key = lambda user: -user.ranking)
-    return render_template('ranking.html', users = ordered_ranking)
+                             key=lambda user: -user.ranking)
+    return render_template('ranking.html', users=ordered_ranking)
+
 
 @app.route('/addOneTournament')
 def addOneTournament():
     global player_tournament
     player_tournament += 1
-    #~ return render_template('tournament.html', users = users, nb_select = player_tournament)
     return redirect('/tournament')
-        
-@app.route('/tournament', methods=['GET','POST'])
+
+
+@app.route('/tournament', methods=['GET', 'POST'])
 def tournament():
+    """
+        Called when trying to create a tournament.
+    """
     users = compute_ranking()
     if request.method == 'POST':
         players = []
@@ -70,53 +65,52 @@ def tournament():
             id_player = request.form[idp]
             user = User.query.filter_by(id_user=id_player).first()
             players.append(user)
-    
+
         tournament = generate_tournament(players)
-        
-        return render_template('tournament.html', users = users, nb_select = player_tournament, tournament = tournament)
-    return render_template('tournament.html', users = users, nb_select = player_tournament)
+
+        return render_template('tournament.html', users=users, nb_select=player_tournament, tournament=tournament)
+    return render_template('tournament.html', users=users, nb_select=player_tournament)
+
 
 @app.route('/ranking_graph')
 def ranking_graph():
 
     now = datetime.now()
-    if(now.month-5 != 0):
-        date = now.replace(month = now.month-5)
+    if now.month-5 != 0:
+        date = now.replace(month=now.month-5)
     else:
-        date = now.replace(month =12).replace(year = now.year-1)
+        date = now.replace(month=12).replace(year=now.year-1)
     date_score = get_ranking_at_timet(date)
     date_score = OrderedDict(sorted(date_score.items(), key=lambda t: t[0]))
     title = 'Monthly Ranking Evolution'
-    line_chart = pygal.Line(title = title, fill = True)
-    line_chart.x_labels = [ str(date)[0:7] for date in date_score.keys() ]
-    
+    line_chart = pygal.Line(title=title, fill=True)
+    line_chart.x_labels = [str(date)[0:7] for date in date_score.keys()]
+
     users_base = User.query.all()
-    
+
     for user_base in users_base:
         user_array = []
         for date, users in date_score.items():
             for user in users:
-                if(user.id_user == user_base.id_user):
+                if user.id_user == user_base.id_user:
                     user_array.append(user.ranking)
         line_chart.add(user_base.nickname, user_array)
 
-    return render_template('ranking_graph.html', line_chart = line_chart)
-    
-    
-    
-    
+    return render_template('ranking_graph.html', line_chart=line_chart)
+
+
 @app.route('/add_match')
 def add_match():
     """Allows user to access the match adding form."""
 
     users = User.query.all()
 
-    if(len(users) == 0):
+    if len(users) == 0:
         success = "There are no players yet !"
-        return render_template('add_match.html', success = success,
-                               user = True)
+        return render_template('add_match.html', success=success,
+                               user=True)
 
-    return render_template('add_match.html', users = users)
+    return render_template('add_match.html', users=users)
 
 
 @app.route('/new_match', methods=['POST'])
@@ -158,7 +152,7 @@ def new_match():
     elif not score_e2:
         error = 'Add a score for Team 2'
 
-    elif(not(score_e1.isdigit()) or not(score_e2.isdigit())):
+    elif not(score_e1.isdigit()) or not(score_e2.isdigit()):
         error = 'Please give integer values for score !'
 
     else:
@@ -176,8 +170,7 @@ def new_match():
 
         success = "Match was successfully added "
 
-
-    return render_template('add_match.html', success= success,
+    return render_template('add_match.html', success=success,
                            error=error, users=users)
 
 
@@ -212,7 +205,7 @@ def add_player():
 
         else:
             # Check if user already exists
-            cur = User.query.filter_by(nickname = request.form['nickname']).first()
+            cur = User.query.filter_by(nickname=request.form['nickname']).first()
             if(cur):
                 error = "This nickname is already used !"
             else:
@@ -260,14 +253,14 @@ def compute_ranking():
         elo(match.player11, match.player12, match.player21, match.player22,
             match.score_e1, match.score_e2)
 
-    #~ users = User.query.all()
+    # users = User.query.all()
 
     return users
 
 
 def elo(me, my_friend, my_ennemy1, my_ennemy2, my_score, opponent_score):
     # Create fictive player1
-    if(my_friend is None):
+    if my_friend is None:
         elo1 = me.ranking
         number_match1 = me.number_of_match
     else:
@@ -275,7 +268,7 @@ def elo(me, my_friend, my_ennemy1, my_ennemy2, my_score, opponent_score):
         number_match1 = (me.number_of_match+my_friend.number_of_match)/2
 
     # Create fictive player2
-    if(my_ennemy2 is None):
+    if my_ennemy2 is None:
         elo2 = my_ennemy1.ranking
         number_match2 = my_ennemy1.number_of_match
     else:
@@ -298,7 +291,7 @@ def elo(me, my_friend, my_ennemy1, my_ennemy2, my_score, opponent_score):
     score_p2 = K*G*(W-We2)
 
     # Get points to real users
-    if(my_friend is None):
+    if my_friend is None:
         me.ranking += int(round(score_p1, 0))
         me.number_of_match += 1
     else:
@@ -351,25 +344,29 @@ def chooseW(score_e1, score_e2):
 
 def p(i):
     return 1/(1+10**(-i/400))
-   
+
+
 def generate_tournament(participants):
+    """
+        Create a tournament with the participants given in parameter.
+    """
     number_of_participants = len(participants)
-    if(number_of_participants%2 != 0):
+    if(number_of_participants % 2 != 0):
         raise Exception("You must be a power of 2 !")
-                
-    #get the list of all players
+
+    # get the list of all players
     players = participants
-    
+
     # ordered the list of all players
-    players = sorted(players, key=lambda player: player.ranking)  
-    
+    players = sorted(players, key=lambda player: player.ranking)
+
     # create the team
-    teams = [(players[i], players[len(players)-1-i]) 
-                for i in range(math.floor(len(players)/2))]    
-        
+    teams = [(players[i], players[len(players)-1-i])
+             for i in range(math.floor(len(players)/2))]
+
     # ordered the team
     teams = sorted(teams, key=lambda team: (team[0].ranking+team[1].ranking)/2)
-    
+
     index = 1
     for team in teams:
         index += 1
@@ -380,45 +377,54 @@ def generate_tournament(participants):
     # create the tournament to make distance between contenders
     for i in range(math.floor(len(teams)/2)):
         if(first):
-            tournament_head.append(((teams[i]),teams[len(teams)-1-i]))
+            tournament_head.append(((teams[i]), teams[len(teams)-1-i]))
         else:
-            tournament_queue.insert(0,((teams[i]),teams[len(teams)-1-i]))
+            tournament_queue.insert(0, ((teams[i]), teams[len(teams)-1-i]))
         first = not first
-        
+
     tournament = tournament_head + tournament_queue
     print("TOURNAMENT")
     for (team1, team2) in tournament:
         print(team1[0].name, team1[0].ranking)
         print(team1[1].name, team1[1].ranking)
-        print((team1[0].ranking+team1[1].ranking)/2)        
+        print((team1[0].ranking+team1[1].ranking)/2)
         print(team2[0].name, team2[0].ranking)
         print(team2[1].name, team2[1].ranking)
-        print((team2[0].ranking+team2[1].ranking)/2)        
+        print((team2[0].ranking+team2[1].ranking)/2)
         print()
     return tournament
 
-        
+
 def all_pairs(lst):
     if len(lst) < 2:
         yield lst
         return
     a = lst[0]
-    for i in range(1,len(lst)):
-        pair = (a,lst[i])
+    for i in range(1, len(lst)):
+        pair = (a, lst[i])
         for rest in all_pairs(lst[1:i]+lst[i+1:]):
             yield [pair] + rest
-    
+
+
 def build_avg_temp(pairs, participants):
-    s = []
+    """
+        Create a array with the average elo for each team
+    """
+    avg_array = []
     for pair in pairs:
         temp_avg = (participants[pair[0]].ranking+participants[pair[1]].ranking)/2
-        s.append(temp_avg)
-    return s
-     
+        avg_array.append(temp_avg)
+    return avg_array
+
+
 def get_ranking_at_timet(date):
-    
+    """
+        This method generate a dict with date as key and list of user
+        as values. Use this method to generate a chart of the ranking
+        evolution.
+    """
     date_score = {}
-    
+
     # Compute the elo at time date
     users = User.query.all()
     for user in users:
@@ -430,32 +436,27 @@ def get_ranking_at_timet(date):
     for match in matchs:
         elo(match.player11, match.player12, match.player21, match.player22,
             match.score_e1, match.score_e2)
-    
+
     date_score[date] = deepcopy(users)
 
     # Get the match per month
     matchs = Match.query.filter(Match.date >= date).all()
-        
+
     groups_match = groupby(matchs, key=lambda x: x.date.timetuple().tm_mon)
-    
-    
+
     for month, match_per_month in groups_match:
         if(date.month > month):
-            date = date.replace(year = date.year+1)
-        date = date.replace(month = month)
+            date = date.replace(year=date.year+1)
+        date = date.replace(month=month)
         for match in match_per_month:
             elo(match.player11, match.player12, match.player21, match.player22,
                 match.score_e1, match.score_e2)
         date_score[date] = deepcopy(users)
     return date_score
 
-@app.route('/coucou')
-def coucou():
-    date = datetime.now()
-    get_ranking_at_timet(date)
-    return "j"
-    
+
 def init_db():
+    """ Initialize the database. """
     db.create_all()
 
 if __name__ == '__main__':
